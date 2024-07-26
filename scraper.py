76c79046bb694fda0abab5b6cd2c3f79c4b1cc51
Todd Pickell <todd@tap-software.com>
2024-07-26 00:09:25 -0500

    a little smarter

--- scraper.py
+++ scraper.py
@@ -1,53 +1,84 @@
 """
 Module Doc String
 """
+import csv
+import datetime
 import json
+from pathlib import Path
 import pprint
 import re
 from linkedin_api import Linkedin
 
-with open("credentials.json", "r") as f:
+def company_name(company_data):
+    "Get the company name from the nested company_data strucutre"
+    return company_data['com.linkedin.voyager.deco.jobs.web.shared.WebCompactJobPostingCompany']['companyResolutionResult']['name']
+
+def apply_url(apply_data):
+    "unwraps the apply data structure to get at the apply url"
+    unwrapped = list(apply_data.values())[0]
+    return list(unwrapped.values())[1]
+
+with open("credentials.json", "r", encoding="utf-8") as f:
     credentials = json.load(f)
+    file_path = f"results_{str(datetime.datetime.now()).replace(" ", "_")}.csv"
+    Path(file_path).touch()
 
     if credentials:
         linkedin = Linkedin(credentials["username"], credentials["password"])
+        with open(file_path, 'w', encoding="utf-8", newline='') as csv_file:
+            field_names = ['title', 'technology', 'posting_id', 'apply_method', 'company']
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(field_names)
 
-        found_langs = {}
-        for offset in range(21):
-            for job in linkedin.search_jobs(
-                keywords="software engineer",
-                experience=["4"],
-                job_type=["F", "C"],
-                location_name="United States",
-                remote=["2"],
-                limit=49,
-                offset=offset
-            ):
-                job_id = job["entityUrn"].split(":")[3]
-                job_data = linkedin.get_job(job_id=job_id)
-                description_text = job_data["description"]["text"].lower()
-                print("Job Found:")
-                pprint.pp(job_data["title"])
-                pprint.pp(job_data["jobPostingId"])
+            found_langs = {}
+            for offset in range(21):
+                for job in linkedin.search_jobs(
+                    keywords="software engineer",
+                    experience=["4"],
+                    job_type=["F", "C"],
+                    location_name="United States",
+                    remote=["2"],
+                    limit=49,
+                    offset=offset
+                ):
+                    job_id = job["entityUrn"].split(":")[3]
+                    job_data = linkedin.get_job(job_id=job_id)
+                    description_text = job_data["description"]["text"].lower()
+                    print("Job Found:")
+                    title = job_data["title"]
+                    posting_id = job_data["jobPostingId"]
+                    company = company_name(job_data["companyDetails"])
+                    apply_method = apply_url(job_data["applyMethod"])
+                    pprint.pp(title)
+                    pprint.pp(posting_id)
 
-                langs = ["ruby", "elixir", "node.js", "typescript", "golang", "go", "python",
-                         "java", "clojure", "scala", "haskell", "lisp", "rust", "swift",
-                         "kotlin", "c++", "c#", "react", "vuejs", "javascript", "elm",
-                         "groovy", "f#", "ocaml", "smalltalk", "scheme", "racket", "php",
-                         "objective-c", "flutter", "dart", "lua", "perl", "erlang", "gleam",
-                         "clojerl", "hcl", "crystal", "julia", "zig", "fortran", "cobol",
-                         "prolog", "ada", "nim", "postgresql", "mysql", "mongodb", "redis",
-                         "mariadb", "elasticsearch", "snowflake", "neo4j", "flask", "django",
-                         "angular", "phoenix", "rails", ".net", "pytorch", "spring", "gql"]
+                    langs = ["ruby", "elixir", "node.js", "typescript", "golang", "go", "python",
+                             "java", "clojure", "scala", "haskell", "lisp", "rust", "swift",
+                             "kotlin", "c++", "c#", "react", "vuejs", "javascript", "elm",
+                             "groovy", "f#", "ocaml", "smalltalk", "scheme", "racket", "php",
+                             "objective-c", "flutter", "dart", "lua", "perl", "erlang", "gleam",
+                             "clojerl", "hcl", "crystal", "julia", "zig", "fortran", "cobol",
+                             "prolog", "ada", "nim", "postgresql", "mysql", "mongodb", "redis",
+                             "mariadb", "elasticsearch", "snowflake", "neo4j", "flask", "django",
+                             "angular", "phoenix", "rails", ".net", "pytorch", "spring", "gql",
+                             "ios", "android", "concurency", "senior"]
 
-                for lang in langs:
-                    reg = r"\s%s[\s,]" % re.escape(lang)
-                    maybe_found = re.findall(reg, description_text)
-                    if maybe_found != []:
-                        [l, *_] = maybe_found
-                        l_key = l.strip(",").strip()
-                        found_langs.setdefault(l_key, 0)
-                        found_langs[l_key] += 1
-                        print("%s: %s" % (l_key, found_langs[l_key]))
+                    technology = []
+                    for lang in langs:
+                        reg = fr"\s{re.escape(lang)}[\s,]"
+                        maybe_found = re.findall(reg, description_text)
+                        if maybe_found != []:
+                            [l, *_] = maybe_found
+                            l_key = l.strip(",").strip()
+                            found_langs.setdefault(l_key, 0)
+                            found_langs[l_key] += 1
+                            technology.append(l_key)
+                            print(f"{l_key}: {found_langs[l_key]}")
 
-        pprint.pp(found_langs)
+                    csv_writer.writerow([title,
+                         str(technology),
+                         posting_id,
+                         str(apply_method),
+                         str(company)])
+
+            pprint.pp(found_langs)
